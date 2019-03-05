fe4b08393eb1d40f559492e77c1aecdc1702d889
Phuong Nguyen (UWL) <nguyen.phuo@uwlax.edu>
2019-03-04 21:56:03 -0600

    add Pattern, add Node

--- tigerNet_Front/src/app/component/main/main.component.ts
+++ tigerNet_Front/src/app/component/main/main.component.ts
@@ -28,192 +28,13 @@ export class MainComponent implements OnInit, AfterViewInit {
 
 
     ngOnInit() {
-      let elements: any[] = [];
-      let nonConnectorSelectors = "";
-      let inactiveSelectors = "";
-      let isConnectorSelectors = "";
+
       this.data.getNetwork().subscribe((res: HttpResponse<Network>) => {
           if (!res.ok) {
               alert("Error loading the network");
               return;
           }
-          // this.network = new Network(res.body.patterns, res.body.patternConnections);
-          //this.network = <Network>res.body
-          this.network = new Network(res.body.patterns, res.body.patternConnections);
-          this.network.patterns.forEach( (pattern: Pattern) => {
-              elements.push({
-                  data: {
-                      id: pattern.id
-                  }
-              })
-              pattern.nodes.forEach(( node: Node) => {
-                  elements.push({
-                      data: {
-                          id: node.id,
-                          parent: pattern.id
-                      }
-                  });
-                  if(node.isActive) {
-                      if(node.isConnector) {
-                          isConnectorSelectors += "#" + node.id + ",";
-                      } else {
-                          nonConnectorSelectors += "#" + node.id + ",";
-                      }
-                  } else {
-                      inactiveSelectors += "#" + node.id + ",";
-                  }
-              });
-              pattern.connections.forEach(( connection: Connector) => {
-                  elements.push({
-                      // data: {
-                      //     id: "" + connection.node + connection.other,
-                      //     source: "" + connection.node,
-                      //     target: "" + connection.other
-                      // }
-                      data: {
-                          id: "" + connection.id + connection.targetId,
-                          source: "" + connection.id,
-                          target: "" + connection.targetId
-                      }
-                  });
-              });
-          });
-          // this.network.patternConnections.forEach((pConnection: Connector) => {
-          //     let pid: string = pConnection.id;
-          //     let otherPid: string = pConnection.targetId;
-          //     let pattern: Pattern = this.network.getPatternById(pid);
-          //     if(!pattern) {
-          //         alert( "no connector node found in pattern" );
-          //     }
-          //     let otherPattern: Pattern = this.network.getPatternById(otherPid);
-          //     if(!otherPattern) {
-          //         alert( "no connector node found in pattern" );
-          //     }
-          //     let connectorNode: Node = pattern.getConnectorNode();
-          //     if(!connectorNode) {
-          //         alert( "no connector node found in pattern" );
-          //     }
-          //     let otherConnectorNode: Node = otherPattern.getConnectorNode();
-          //     if(!otherConnectorNode) {
-          //         alert( "no connector node found in pattern" );
-          //     }
-          //     elements.push({
-          //         data: {
-          //             id: connectorNode.id + otherConnectorNode.id,
-          //             source: connectorNode.id,
-          //             target: otherConnectorNode.id
-          //         }
-          //     })
-          // });
-          this.network.patternConnections.forEach((pConnection: Connector) => {
-              // let pid: string = pConnection.id;
-              // let otherPid: string = pConnection.targetId;
-              // let pattern: Pattern = this.network.getPatternById(pid);
-              // if(!pattern) {
-              //     alert( "no connector node found in pattern" );
-              // }
-              // let otherPattern: Pattern = this.network.getPatternById(otherPid);
-              // if(!otherPattern) {
-              //     alert( "no connector node found in pattern" );
-              // }
-              // let connectorNode: Node = pattern.getConnectorNode();
-              // if(!connectorNode) {
-              //     alert( "no connector node found in pattern" );
-              // }
-              // let otherConnectorNode: Node = otherPattern.getConnectorNode();
-              // if(!otherConnectorNode) {
-              //     alert( "no connector node found in pattern" );
-              // }
-              // elements.push({
-              //     data: {
-              //         id: connectorNode.id + otherConnectorNode.id,
-              //         source: connectorNode.id,
-              //         target: otherConnectorNode.id
-              //     }
-              // });
-              elements.push({
-                  data: {
-                      id: pConnection.id + pConnection.targetId,
-                      source: pConnection.id,
-                      target: pConnection.targetId
-                  }
-              });
-          });
-          nonConnectorSelectors = nonConnectorSelectors.substr(0, nonConnectorSelectors.length - 1);
-          inactiveSelectors = inactiveSelectors.substr(0, inactiveSelectors.length - 1);
-          isConnectorSelectors = isConnectorSelectors.substr(0, isConnectorSelectors.length - 1);
-
-          this.cy = cytoscape({
-              container: document.getElementById('cy'), // container to render in
-              elements: elements,
-              style: [ // the stylesheet for the graph
-                  {
-                      selector: 'node',
-                      style: {
-                          //'background-color': '#1C86EE',
-                          'label': 'data(id)'
-                      }
-                  },
-                  {
-                      selector: 'edge',
-                      style: {
-                          'width': 2,
-                          'line-color': '#000'
-                      }
-                  },
-                  {
-                      selector: isConnectorSelectors,
-                      style: {
-                          'background-color': '#FFD700'
-                      }
-                  },
-                  {
-                      selector: nonConnectorSelectors,
-                      style: {
-                          'background-color': '	#66CD00'
-                      }
-                  },
-                  {
-                      selector: inactiveSelectors,
-                      style: {
-                          'background-color': '	#888888'
-                      }
-                  },
-                  {
-                      selector:  ':selected',
-                      css :{
-                        'background-color' : '#FF4500',
-                      }
-                  }
-              ],
-
-              layout: {
-                  name: 'cose-bilkent',
-                  rows: 1
-              }
-
-          });
-          cytoscape.use(coseBilkent);
-          //var collector = this.cy.collection();
-          //console.log(this);
-          var _this = this;
-
-          this.cy.nodes().on('tap', function(e){
-            //let clickedNode : string;
-            var clickedEle = e.target.id();
-            //console.log();
-            //collector = collector.union(clickedNode);
-            if(clickedEle.charAt(0) === 'P' && ! _this.data.selectedPatterns.includes(clickedEle))
-              _this.data.selectedPatterns.push(clickedEle);
-            else if(clickedEle.charAt(0) === 'N' && ! _this.data.selectedNodes.includes(clickedEle))
-              _this.data.selectedNodes.push(clickedEle);
-          });
-
-          this.cy.on('tap','edge', function(e){
-            //let clickedNode : string;
-            var clickedEle = e.target.id();
-            _this.data.selectedLink.push();
-          });
+          this.resetGraph(res.body.patterns, res.body.patternConnections);
       });
     }
 
@@ -279,7 +100,7 @@ export class MainComponent implements OnInit, AfterViewInit {
       }
 
       // If selected pattern has 3 nodes, then must select at least 2 node.
-      if(currentPattern.nodes.length >= 3){
+      if(currentPattern.nodes.length >= 2){
         if(this.data.selectedNodes.length < 2){
           this.resetSelectedElement();
           console.log("hit");
@@ -288,12 +109,15 @@ export class MainComponent implements OnInit, AfterViewInit {
         }
       }
 
-      var reqObject = { 'pattern' : currentPattern,
-                        'nodes' : this.data.selectedNodes};
+      var reqObject = { 'pattern' : currentPattern.id,
+                        'nodes' : this.data.selectedNodes,
+                        'conNode' : currentPattern.getConnectorNode().id,
+                        'currentNodeNum' : currentPattern.nodes.length};
       this.resetSelectedElement();
 
       this.data.addNode(reqObject).subscribe( (data) =>{
         console.log(data);
+        this.resetGraph(data.patterns, data.patternConnections);
       });
     }
 
@@ -361,7 +185,7 @@ export class MainComponent implements OnInit, AfterViewInit {
       }
       else{
         var arrToSend = [];
-        console.log(this.data.selectedPatterns);
+        //console.log(this.data.selectedPatterns);
         this.data.selectedPatterns.forEach( (pid) =>{
           arrToSend.push(this.network.getPatternById(pid).getConnectorNode().id);
         });
@@ -369,7 +193,143 @@ export class MainComponent implements OnInit, AfterViewInit {
         this.resetSelectedElement();
         this.data.addPattern(arrToSend).subscribe( (data) =>{
           console.log(data);
+          this.resetGraph(data.patterns, data.patternConnections);
         });
       }
+    }
+
+    resetGraph( patterns, connections){
+      let elements: any[] = [];
+      let nonConnectorSelectors = "";
+      let inactiveSelectors = "";
+      let isConnectorSelectors = "";
+
+      this.network = new Network(patterns, connections);
+      this.network.patterns.forEach( (pattern: Pattern) => {
+          elements.push({
+              data: {
+                  id: pattern.id
+              }
+          })
+          pattern.nodes.forEach(( node: Node) => {
+              elements.push({
+                  data: {
+                      id: node.id,
+                      parent: pattern.id
+                  }
+              });
+              if(node.isActive) {
+                  if(node.isConnector) {
+                      isConnectorSelectors += "#" + node.id + ",";
+                  } else {
+                      nonConnectorSelectors += "#" + node.id + ",";
+                  }
+              } else {
+                  inactiveSelectors += "#" + node.id + ",";
+              }
+          });
+          pattern.connections.forEach(( connection: Connector) => {
+              elements.push({
+                  // data: {
+                  //     id: "" + connection.node + connection.other,
+                  //     source: "" + connection.node,
+                  //     target: "" + connection.other
+                  // }
+                  data: {
+                      id: "" + connection.id + connection.targetId,
+                      source: "" + connection.id,
+                      target: "" + connection.targetId
+                  }
+              });
+          });
+      });
+      this.network.patternConnections.forEach((pConnection: Connector) => {
+          elements.push({
+              data: {
+                  id: pConnection.id + pConnection.targetId,
+                  source: pConnection.id,
+                  target: pConnection.targetId
+              }
+          });
+      });
+      nonConnectorSelectors = nonConnectorSelectors.substr(0, nonConnectorSelectors.length - 1);
+      inactiveSelectors = inactiveSelectors.substr(0, inactiveSelectors.length - 1);
+      isConnectorSelectors = isConnectorSelectors.substr(0, isConnectorSelectors.length - 1);
+
+      this.cy = cytoscape({
+          container: document.getElementById('cy'), // container to render in
+          elements: elements,
+          style: [ // the stylesheet for the graph
+              {
+                  selector: 'node',
+                  style: {
+                      //'background-color': '#1C86EE',
+                      'label': 'data(id)'
+                  }
+              },
+              {
+                  selector: 'edge',
+                  style: {
+                      'width': 2,
+                      'line-color': '#000'
+                  }
+              },
+              {
+                  selector: isConnectorSelectors,
+                  style: {
+                      'background-color': '#FFD700'
+                  }
+              },
+              {
+                  selector: nonConnectorSelectors,
+                  style: {
+                      'background-color': '	#66CD00'
+                  }
+              },
+              {
+                  selector: inactiveSelectors,
+                  style: {
+                      'background-color': '	#888888'
+                  }
+              },
+              {
+                  selector:  ':selected',
+                  css :{
+                    'background-color' : '#FF4500',
+                  }
+              },
+
+          ],
+
+          layout: {
+              name: 'cose-bilkent',
+              rows: 1
+          }
+
+      });
+      cytoscape.use(coseBilkent);
+      //var collector = this.cy.collection();
+      //console.log(this);
+      var _this = this;
+      //this.cy.$('#' + clickedEle).addClass('test');
+
+
+      this.cy.nodes().on('tap', function(e){
+        //let clickedNode : string;
+        var clickedEle = e.target.id();
+        //console.log();
+
+        //collector = collector.union(clickedNode);
+        if(clickedEle.charAt(0) === 'P' && ! _this.data.selectedPatterns.includes(clickedEle))
+          _this.data.selectedPatterns.push(clickedEle);
+        else if(clickedEle.charAt(0) === 'N' && ! _this.data.selectedNodes.includes(clickedEle))
+          _this.data.selectedNodes.push(clickedEle);
+      });
+
+      this.cy.on('tap','edge', function(e){
+        //let clickedNode : string;
+        var clickedEle = e.target.id();
+        _this.data.selectedLink.push();
+      });
     }
 }
